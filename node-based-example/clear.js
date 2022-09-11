944fb13442b5eefc2484417fa5c81f290d6c8501
Bhupendra <bsingh1324@gmail.com>
2022-09-11 17:49:36 +0530

    Added cesium and node files

---
+++ node-based-example/clear.js
@@ -0,0 +1,121 @@
+const request = require("request-promise");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const accessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjZDVlYzI3Ni01ZTcxLTQzNzEtYjA1MC1lNTQ4MmI3OWI3ZjEiLCJpZCI6OTgyMzAsImlhdCI6MTY1OTY5MTQ4N30.kocZXnKaN5T_JK3f6BxjELLWZLkZ9HwvPl4okUJSUsw"; // Replace this with your token from above.
+
+const getAssetIds = async function (ifcomplete = null) {
+  const response = await request({
+    url: "https://api.cesium.com/v1/assets",
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true,
+  });
+
+  //   console.log(response);
+  const data = await response;
+  return data;
+};
+
+const uploadFiles = async () => {
+  const postBody = {
+    name: "Reichstag",
+    description: "See [Wikipedia](https://en.wikipedia.org/?curid=217577).",
+    type: "3DTILES",
+    options: {
+      sourceType: "CITYGML",
+      clampToTerrain: true,
+      baseTerrainId: 1,
+    },
+  };
+  const response = await request({
+    url: "https://api.cesium.com/v1/assets",
+    method: "POST",
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true,
+    body: postBody,
+  });
+  const data = await response;
+  return data;
+};
+
+const afterUploadReq = async (response) => {
+  const uploadLocation = response.uploadLocation;
+  s3 = new AWS.S3({
+    apiVersion: "2006-03-01",
+    region: "us-east-1",
+    signatureVersion: "v4",
+    endpoint: uploadLocation.endpoint,
+    credentials: new AWS.Credentials(
+      uploadLocation.accessKey,
+      uploadLocation.secretAccessKey,
+      uploadLocation.sessionToken
+    ),
+  });
+  const input = "./Reichstag.zip";
+
+  await s3
+    .upload({
+      Body: fs.createReadStream(input),
+      Bucket: uploadLocation.bucket,
+      Key: `${uploadLocation.prefix}Reichstag.zip`,
+    })
+    .promise()
+    .then((res) => {
+      console.log(res);
+      const onComplete1 = response.onComplete;
+      onComplete(onComplete1, response);
+    });
+};
+
+const onComplete = async (response, mainRes) => {
+  await request({
+    url: response.url,
+    method: response.method,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true,
+    body: response.fields,
+  }).then((r) => {
+    // console.log(r);
+    waitUntilReady(mainRes);
+  });
+  //   console.log("Okay");
+};
+
+async function waitUntilReady(response) {
+  const assetId = response.assetMetadata.id;
+
+  // Issue a GET request for the metadata
+  const assetMetadata = await request({
+    url: `https://api.cesium.com/v1/assets/${assetId}`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true,
+  });
+
+  const status = assetMetadata.status;
+  if (status === "COMPLETE") {
+    console.log("Asset tiled successfully");
+    console.log(
+      `View in ion: https://cesium.com/ion/assets/${assetMetadata.id}`
+    );
+  } else if (status === "DATA_ERROR") {
+    console.log("ion detected a problem with the uploaded data.");
+  } else if (status === "ERROR") {
+    console.log(
+      "An unknown tiling error occurred, please contact support@cesium.com."
+    );
+  } else {
+    if (status === "NOT_STARTED") {
+      console.log("Tiling pipeline initializing.");
+    } else {
+      // IN_PROGRESS
+      console.log(`Asset is ${assetMetadata.percentComplete}% complete.`);
+    }
+
+    // Not done yet, check again in 10 seconds
+    setTimeout(function () {
+      waitUntilReady(response);
+    }, 10000);
+    // setTimeout(waitUntilReady, 10000);
+  }
+}
+module.exports = { getAssetIds, uploadFiles, afterUploadReq };
---
+++ node-based-example/public/index.js
@@ -0,0 +1,96 @@
+var viewer = new Cesium.Viewer("cesiumContainer");
+const loader = document.querySelector("#loading");
+viewer.infoBox.frame.sandbox =
+  "allow-same-origin allow-top-navigation allow-pointer-lock allow-popups allow-forms allow-scripts";
+$("#load").on("click", () => {
+  loader.classList.add("display");
+  fetch("/assetIds")
+    .then((resp) => resp.json())
+    .then((data) => populateAssetIds(data));
+});
+
+const populateAssetIds = (ids) => {
+  const selectDiv = $("#assetIDs");
+  let optionIds = "";
+  ids["items"].forEach((element) => {
+    optionIds += `<option>${element["name"]} {${element["id"]}}</option>`;
+  });
+  selectDiv.append(optionIds);
+  loader.classList.remove("display");
+};
+
+const loadAssetId = (assetId) => {
+  let resourceId = +$("#cesiumResource").val();
+  //   let token = $("#cesiumToken").val();
+  let token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiZGU1NGNhYy1jN2MyLTQ2YTAtOTkzZS05OTUzOGZjNjliY2MiLCJpZCI6OTgyMzAsImlhdCI6MTY1NTcxMTEyNH0.c2_6dE0vgWs0lHZDfBtD4nLZpCXPCOII7gYdXjtIfao";
+  Cesium.Ion.defaultAccessToken = token;
+  const tileset = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+      url: Cesium.IonResource.fromAssetId(assetId),
+    })
+  );
+
+  (async () => {
+    try {
+      await tileset.readyPromise;
+      await viewer.zoomTo(tileset);
+
+      // Apply the default style if it exists
+      var extras = tileset.asset.extras;
+      if (
+        Cesium.defined(extras) &&
+        Cesium.defined(extras.ion) &&
+        Cesium.defined(extras.ion.defaultStyle)
+      ) {
+        tileset.style = new Cesium.Cesium3DTileStyle(extras.ion.defaultStyle);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+};
+
+$("#show").on("click", () => {
+  let assetVal = $("#assetIDs").val();
+  if (assetVal != "Select Assest IDs...") {
+    let assetId = assetVal.split("{")[1].split("}")[0];
+    fetch("/assetid/" + assetId, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ assetId: assetId }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => checkIfTiled(data, assetId));
+  } else {
+    alert("Please load assetIds or select one!!");
+  }
+});
+
+const checkIfTiled = (data, assetId) => {
+  if (data.status == "COMPLETE") {
+    loadAssetId(assetId);
+  } else {
+    alert(`The titling is ${data.status}. Please try again later`);
+  }
+};
+
+const loadLocalFiles = () => {
+  alert("Hello");
+  let token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiZGU1NGNhYy1jN2MyLTQ2YTAtOTkzZS05OTUzOGZjNjliY2MiLCJpZCI6OTgyMzAsImlhdCI6MTY1NTcxMTEyNH0.c2_6dE0vgWs0lHZDfBtD4nLZpCXPCOII7gYdXjtIfao";
+  var viewer = new Cesium.Viewer("cesiumContainer");
+  var tileset = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+      url: "http://localhost:3000/tiles/tileset.json", // URL from `Starting the Server` section.
+    })
+  );
+  viewer.zoomTo(tileset);
+};
+
+$("#load_local").on("click", () => {
+  loadLocalFiles();
+});
